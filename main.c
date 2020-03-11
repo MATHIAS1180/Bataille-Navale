dedb4317a3ca190a68fe20c835529e42d7f5111f
BOURQUI Mathias <Mathias.BOURQUI@cpnv.ch>
2020-03-11 12:15:32 +0100

    Commenecement du code bataille navale

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+int menu() {
+    int valeur
+    printf("Bataille Navale\n");
+    printf("\n_______________\n");
+    scanf()return valeur ;
+}
+
+
+
 
 int main() {
-
+int valeur;
+   valeur = menu();
     return 0;
 }
