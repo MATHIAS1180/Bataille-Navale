7a776410d28a55ffa7ab0643d60f70bebe394acc
BOURQUI Mathias <Mathias.BOURQUI@cpnv.ch>
2020-03-13 14:11:52 +0100

    Ajout des pause dans les variables

--- Bataille navale 0.1/main.c
+++ Bataille navale 0.1/main.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <windows.h>
+
 #pragma set_character_execution("UTF-8")
 
 int choixmenu;
@@ -9,39 +10,35 @@ int choixmenu;
 void jeu() {
     int InterfaceDeJeu();
     system("cls");
-        printf("--- Bataille Navale ---\n\n");
-        printf("     { 1 }  { 2 }  { 3 }  { 4 }  { 5 }  { 6 }  { 7 }  { 8 }  { 9 } { 1 0 } \n");
-        for(char i = 'A'; i < 'K'; i++) {
-            printf("\n------------------------------------------------------------------------------\n");
-            printf("%c    ", i);
-            printf("{ }    ");
-            for(char e = 0; e < 5 ; e++){
-                printf("{ }    { }    ");
-            }
+    printf("-------Bataille Navale ------\n\n");
+    printf("     { 1 }  { 2 }  { 3 }  { 4 }  { 5 }  { 6 }  { 7 }  { 8 }  { 9 } { 1 0 } \n");
+    for (char i = 'A'; i < 'K'; i++) {
+        printf("\n------------------------------------------------------------------------------\n");
+        printf("%c    ", i);
+        printf("{ }    ");
+        for (char e = 0; e < 5; e++) {
+            printf("{ }    { }    ");
         }
-        system("Pause");
     }
+    system("Pause");
+}
 
-
-void pseudo(){
-    char pseudo;
+//Demande de choisir un pseudo
+int pseudo() {
     int retour;
-    do {
-        system("cls");
-        printf("Choisisez un pseudo :");
-        scanf("%c", &pseudo);
-        printf("Daccord %c !", pseudo);
-
-        scanf("%d", &retour);
-    }
-    while(retour !=1);
+    char pseudo[20];
+    system("cls");
+    printf("            ------- Pseudo -------\n");
+    printf("Quel est votre pseudo:");
+    scanf("%s", pseudo);
+    printf("Daccord %s !\n", pseudo);
     system("pause");
 }
 
+
+
 //Affiche l'aide de jeu
-void aide() {
-    int retour;
-    do {
+    void aide() {
         system("cls");
         printf("\n------Aide------\n");
         printf("\n"
@@ -59,52 +56,49 @@ void aide() {
                "\nUn bateau est coulé quand toutes ses cases sont touchées\n"
 
                "Une partie se termine quand un joueur coule tout les bateaux adverses.\n");
-        printf("\nAppuyer sur 1 pour retourner au menu :");
-        scanf("%d",&retour);
-    } while (retour != 1);
-
-}
-
-//Affiche le menu et redirige vers la redirection de notre choix.
-int menu() {
-    system("cls");
-    printf("\nBataille Navale");
-    printf("\n1-Jouer\n");
-    printf("2-Pseudo\n");
-    printf("3-Scores\n");
-    printf("4-Aide\n");
-    printf("5-Quitter\n");
-    printf("\nFaite votre choix :");
-    scanf("%d", &choixmenu);
-    fflush(stdin);
-    switch (choixmenu) {
-        case 1 :
-            jeu();
-            break;
-        case 2 :
-            pseudo();
-            break;
-        case 3 :
-            break;
-        case 4 :
-            aide();
-            break;
-        case 5 :
-            break;
-        default :
-            system("cls");
-            break;
+        system("pause");
     }
-}
+//Affiche le menu et redirige vers la redirection de notre choix.
+    int menu() {
+        system("cls");
+        printf("\nBataille Navale");
+        printf("\n1-Jouer\n");
+        printf("2-Pseudo\n");
+        printf("3-Scores\n");
+        printf("4-Aide\n");
+        printf("5-Quitter\n");
+        printf("\nFaite votre choix :");
+        scanf("%d", &choixmenu);
+        fflush(stdin);
+        switch (choixmenu) {
+            case 1 :
+                jeu();
+                break;
+            case 2 :
+                pseudo();
+                break;
+            case 3 :
+                break;
+            case 4 :
+                aide();
+                break;
+            case 5 :
+                break;
+            default :
+                system("cls");
+                break;
+        }
+    }
 
-int main() {
+    int main() {
 
-    SetConsoleOutputCP(65001);
-    do {
-        menu();
-    } while (choixmenu != 5);
+        SetConsoleOutputCP(65001);
+        do {
+            menu();
+        } while (choixmenu != 5);
 
-    return 0;
+        return 0;
 
-}
+    }
+
 
