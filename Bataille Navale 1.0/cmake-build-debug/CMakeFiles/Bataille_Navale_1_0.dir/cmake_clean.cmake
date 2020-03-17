6fa328765062ce2a2b76485812caf70c1d6c9c41
MATHIAS1180 <61789037+MATHIAS1180@users.noreply.github.com>
2020-03-17 11:45:30 +0100

    Ajout du code final saans les commentaires
    j'ai tout copier coller dans un nouveaux project car le cmd a été suprimé.

---
+++ Bataille Navale 1.0/cmake-build-debug/CMakeFiles/Bataille_Navale_1_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Bataille_Navale_1_0.exe"
+  "Bataille_Navale_1_0.exe.manifest"
+  "Bataille_Navale_1_0.pdb"
+  "CMakeFiles/Bataille_Navale_1_0.dir/main.c.obj"
+  "libBataille_Navale_1_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Bataille_Navale_1_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
